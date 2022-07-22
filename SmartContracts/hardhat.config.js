397e95208c40551b48df770e0965a572a40eba4f
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2022-07-22 15:45:10 +0100

    Add env vars

--- SmartContracts/hardhat.config.js
+++ SmartContracts/hardhat.config.js
@@ -2,15 +2,8 @@
 // REQUIRES
 ///////////////////////////////////////////////////////////
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
-///////////////////////////////////////////////////////////
-// CONFIGURATION
-///////////////////////////////////////////////////////////
-
-//TODO: Security Best Practice: Set these values to "" before committing to git
-const PRIVATE_KEY = "";          // Populate from MetaMask, after sign-in
-const MUMBAI_NETWORK_URL = "";   // Populate from admin.moralis.io, after sign-in
-const POLYGONSCAN_API_KEY = "";  // Populate from polygonscan.com, after sign-in
 
 ///////////////////////////////////////////////////////////
 // EXPORTS
@@ -22,12 +15,12 @@ module.exports = {
     solidity: "0.8.9",
     networks: {
         mumbai: {
-            url: MUMBAI_NETWORK_URL,
-            accounts: [PRIVATE_KEY]
+            url: process.env.MUMBAI_NETWORK_URL,
+            accounts: [process.env.PRIVATE_KEY]
         }
     },
     etherscan: {
-        apiKey: POLYGONSCAN_API_KEY
+        apiKey: process.env.POLYGONSCAN_API_KEY
     }
 };
 
