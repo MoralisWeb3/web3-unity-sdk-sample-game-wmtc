1d6e894d45901feea1e38aa518fa3047fdbc076e
Samuel Asher Rivello <74617375+srivelloBeamable@users.noreply.github.com>
2022-07-22 16:41:06 +0100

    Update Structure

--- SmartContracts/HardHat/test/testGreeter.js
+++ SmartContracts/HardHat/test/testGreeter.js
@@ -4,9 +4,9 @@
 const {
     time,
     loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-const { expect } = require("chai");
+  } = require("@nomicfoundation/hardhat-network-helpers");
+  const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
+  const { expect } = require("chai");
 
 
 ///////////////////////////////////////////////////////////
--- SmartContracts/Hardhat/hardhat.config.js
+++ SmartContracts/Hardhat/hardhat.config.js
@@ -4,6 +4,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+// TODO: MOve this to env
+const PRIVATE_KEY = "2f6009cddf4c79754af198995fd9db86f0c4ced09e5e33b8f0d701362f8231d5"; 
 
 ///////////////////////////////////////////////////////////
 // EXPORTS
@@ -11,17 +13,17 @@ require("dotenv").config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
-module.exports = {
-    solidity: "0.8.9",
-    networks: {
-        mumbai: {
-            url: process.env.MUMBAI_NETWORK_URL,
-            accounts: [process.env.PRIVATE_KEY]
-        }
-    },
-    etherscan: {
-        apiKey: process.env.POLYGONSCAN_API_KEY
+ module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    mumbai: {
+      url: process.env.MUMBAI_NETWORK_URL,
+      accounts: [PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.POLYGONSCAN_API_KEY 
+  }
 };
 
 
@@ -30,11 +32,8 @@ module.exports = {
 ///////////////////////////////////////////////////////////
 task("cct", "Clean, Compile, & Test the Greeter.sol").setAction(async () => {
 
-    // Works!
-    await hre.run("clean");
-    await hre.run("compile");
-    await hre.run("test");
+  // Works!
+  await hre.run("clean");
+  await hre.run("compile");
+  await hre.run("test");
 });
-
-
-
